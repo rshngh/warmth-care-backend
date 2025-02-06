64a60286d60901d4542ae54d2bcd8a424e0f1809
rahul <rsngh@outlook.com>
2025-02-06 10:59:31 +0530

    update cookie options

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,7 @@ import asyncHandler from "express-async-handler";
 const cookieOptions = {
   maxAge: 30 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "strict",
+  sameSite: "none",
   secure: true,
 };
 
